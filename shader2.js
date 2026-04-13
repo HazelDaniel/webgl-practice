ef26d9230e5c665c24661784d94f2d42f09986b4
HazelDaniel <hazeldaniel856@gmail.com>
2026-04-13 11:37:42 +0100

    passed random colors from vertex shader to fragment shader using varyings, increased the size range of the rectangles

--- shader2.js
+++ shader2.js
@@ -1,3 +1,19 @@
+function resizeCanvasToDisplaySize(canvas) {
+  const displayWidth = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  const needResize =
+    canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
+
+  return needResize;
+}
+
 function createShader(gl, type, source) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
@@ -7,7 +23,7 @@ function createShader(gl, type, source) {
   if (!status) {
     console.log(`shader source ====:${source}:====`);
     console.error(
-      "shader failed to comiple. reason: ",
+      "shader failed to compile. reason: ",
       gl.getShaderInfoLog(shader)
     );
     gl.deleteShader(shader);
@@ -34,14 +50,15 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
 function generateRectangles(canvas) {
   const rects = [];
+  const colors = [];
   const width = canvas.width;
   const height = canvas.height;
 
   for (let i = 0; i < 50; i++) {
     const x = Math.random() * width;
     const y = Math.random() * height;
-    const w = Math.random() * 80 + 1;
-    const h = Math.random() * 80 + 1;
+    const w = Math.random() * 990 + 1;
+    const h = Math.random() * 990 + 1;
 
     const x2 = x + w;
     const y2 = y + h;
@@ -61,9 +78,16 @@ function generateRectangles(canvas) {
       x2,
       y2
     );
+
+    const color = [Math.random(), Math.random(), Math.random(), 1.0];
+
+    for (let j = 0; j < 6; j++) {
+      colors.push(...color);
+    }
   }
 
   document.getElementById("rect-data").textContent = rects.join(",");
+  document.getElementById("colors-data").textContent = colors.join(",");
 }
 
 function run() {
@@ -91,7 +115,9 @@ function run() {
   generateRectangles(canvas);
 
   const rawData = document.getElementById("rect-data").textContent;
+  const rawColorsData = document.getElementById("colors-data").textContent;
   const vertices = new Float32Array(rawData.split(",")).map(Number);
+  const colors = new Float32Array(rawColorsData.split(",")).map(Number);
 
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
   const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
@@ -100,18 +126,20 @@ function run() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-  // webglUtils.resizeCanvasToDisplaySize();
+  resizeCanvasToDisplaySize(canvas);
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.useProgram(program);
   gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
   gl.enableVertexAttribArray(positionAttributeLocation);
+
   let size = 2;
   let type = gl.FLOAT;
   let normalized = false;
   let stride = 0;
   let offset = 0;
+
   gl.vertexAttribPointer(
     positionAttributeLocation,
     size,
@@ -120,6 +148,16 @@ function run() {
     stride,
     offset
   );
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+  const aColorLocation = gl.getAttribLocation(program, "a_color");
+
+  gl.enableVertexAttribArray(aColorLocation);
+  gl.vertexAttribPointer(aColorLocation, 4, gl.FLOAT, false, 0, 0);
+
   let primitive = gl.TRIANGLES;
   let iteration = vertices.length / 2;
   let offset_ = 0;
